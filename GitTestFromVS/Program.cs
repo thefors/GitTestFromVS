38e691352e4ee857aa8713b94339f42bbb9470f6
Anders Lundholm Fors <mr.fors@gmail.com>
2023-02-22 21:04:35 +0100

    Question added.

--- GitTestFromVS/Program.cs
+++ GitTestFromVS/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.Write("How would you say this in your language? ");
         }
     }
 }
