d812c7664eccd11dc457f167893104c78ee332fa
Anders Lundholm Fors <mr.fors@gmail.com>
2023-02-22 21:13:07 +0100

    added input

--- GitTestFromVS/Program.cs
+++ GitTestFromVS/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.Write("How would you say this in your language? ");
+            string reply = Console.ReadLine();
         }
     }
 }
