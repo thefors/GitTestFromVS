b2f66b7ef4f84a5e3b2225f04689b6eb0f270e8e
Anders Lundholm Fors <mr.fors@gmail.com>
2023-02-22 14:21:03 +0100

    Add project files.

---
+++ GitTestFromVS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitTestFromVS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
